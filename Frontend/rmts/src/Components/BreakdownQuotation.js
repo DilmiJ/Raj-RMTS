25c64cc0d123cc25a7f3680eec5bbaec931feafa
Krishan <krishan142536@gmail.com>
2024-12-03 16:31:37 +0000

    mk

---
+++ Frontend/rmts/src/Components/BreakdownQuotation.js
@@ -0,0 +1,13 @@
+// BreakdownQuotation.js
+import React from 'react';
+
+const BreakdownQuotation = () => {
+  return (
+    <div>
+      <h1>Breakdown Quotation</h1>
+      <p>This is the Breakdown Quotation page.</p>
+    </div>
+  );
+};
+
+export default BreakdownQuotation;
