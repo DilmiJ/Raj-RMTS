7802008eda437ce376d9b64098a2044441e84401
Krishan <krishan142536@gmail.com>
2024-12-30 23:38:00 +0000

    hi

--- Frontend/rmts/src/Components/MonitorView.js
+++ Frontend/rmts/src/Components/MonitorView.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../css/JobSelectionPart.css';
+import '../css/MonitorView.css';
 
 const JobSelectionPart = () => {
   const [activeTab, setActiveTab] = useState(null);
